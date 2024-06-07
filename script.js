7d04d558ba649e4e957f95c7fd3c0a3b74618f2e
KshitijKrCh <2021cs_kshitijkumarchandarakar_a@nie.ac.in>
2024-06-08 02:07:38 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function Display(value) {
     document.getElementById("display").value += value;
+    
 }
 
 function clearDisplay() {
     document.getElementById("display").value = '';
 }
+
 function clear1(){
     let display = document.getElementById("display").value;
     document.getElementById("display").value = display.slice(0, -1);
 }
+
 function calculateResult() {
     try {
         const result = eval(document.getElementById("display").value);
